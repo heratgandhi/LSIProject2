564c4cc163dd87a16b212d690215dc96746c45c4
Herat <hag59@cornell.edu>
2013-04-24 16:51:01 -0400

    block input generation

--- src/MainInputFile.java
+++ src/MainInputFile.java
@@ -4,7 +4,8 @@ public class MainInputFile {
 	public static void main(String[] args) {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("edges1.txt"));
-			BufferedWriter bw = new BufferedWriter(new FileWriter("input"));
+			BufferedReader br1 = new BufferedReader(new FileReader("blocks.txt"));
+			BufferedWriter bw = new BufferedWriter(new FileWriter("blocked_input"));
 			
 			String line = "";
 			long counter = 0;
@@ -12,6 +13,8 @@ public class MainInputFile {
 			String[] parts;
 			String dest = "";
 			long s,d;
+			long currentBlockLimit = Long.parseLong(br1.readLine().trim());
+			long currentBlock = 0;
 			while ( (line=br.readLine()) != null ) {
 				parts = line.split(" ");
 				s = Long.parseLong(parts[0]);
@@ -23,17 +26,21 @@ public class MainInputFile {
 						dest += d;
 					counter ++;
 				} else {
-					bw.write(node+"\t1 "+counter+" "+dest+"\n");
+					bw.write(currentBlock+"\t"+ node + " " + (1/(double)PageRank.nodes) +" "+counter+" "+dest+"\n");
 					dest = d+"";
 					node++;
+					if(node == currentBlockLimit) {
+						currentBlockLimit = Long.parseLong(br1.readLine().trim());
+						currentBlock++;
+					}
 					counter = 1;
 				}
 			}
-			bw.write(node+"\t1 "+counter+" "+dest+"\n");
+			bw.write(currentBlock+"\t"+ node + " " +(1/(double)PageRank.nodes)+" "+counter+" "+dest+"\n");
 			br.close();
 			bw.close();
 		} catch(Exception e) {
-			
+			e.printStackTrace();
 		}
 	}
 }
